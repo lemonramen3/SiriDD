57c4508b2aa1ac5cbc2b8bce7dee551b58fbbc0b
Xinghao <namenamewxh@gmail.com>
2021-03-11 16:38:24 +0800

    Initial Commit

---
+++ 弟弟/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  弟弟
+//
+//  Created by xinghao wang on 2021/3/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 弟弟/__App.swift
@@ -0,0 +1,17 @@
+//
+//  __App.swift
+//  弟弟
+//
+//  Created by xinghao wang on 2021/3/11.
+//
+
+import SwiftUI
+
+@main
+struct __App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
